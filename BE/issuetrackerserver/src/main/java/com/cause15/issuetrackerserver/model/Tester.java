b748fed3d4e9ce0624f454c4324c525d7ecd1ac4
minseok <orgin20@naver.com>
2024-05-18 23:47:40 +0900

    Issue Create API와 충돌, 주석처리

--- BE/issuetrackerserver/src/main/java/com/cause15/issuetrackerserver/model/Tester.java
+++ BE/issuetrackerserver/src/main/java/com/cause15/issuetrackerserver/model/Tester.java
@@ -1,13 +1,11 @@
 package com.cause15.issuetrackerserver.model;
 
-import java.util.UUID;
-
 public class Tester extends User {
     public Tester(String name, String password) {
         super(name, password);
     }
 
-    public Tester(UUID id, String name, String password) {
+    /*public Tester(UUID id, String name, String password) {
         super(id, name, password);
-    }
+    }*/
 }
